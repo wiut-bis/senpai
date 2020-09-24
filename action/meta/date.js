ef2ad266fb6100eadffb204245a96472c426239b
Sokhibjon Orzikulov <genemators@gmail.com>
2020-09-24 16:40:42 +0500

    wholesome update is comin'

---
+++ action/meta/date.js
@@ -0,0 +1,16 @@
+module.exports = async () => {
+  // create Date object for current location
+  const date = new Date();
+
+  // convert to milliseconds, add local time zone offset and get UTC time in milliseconds
+  const utcTime = date.getTime() + date.getTimezoneOffset() * 60000;
+
+  // time offset for Uzbekistan is +5
+  const timeOffset = 5;
+
+  // create new Date object for a different timezone using supplied its GMT offset.
+  const UzbTime = new Date(utcTime + 3600000 * timeOffset);
+
+  // returning week date
+  return UzbTime.getDay()
+};
