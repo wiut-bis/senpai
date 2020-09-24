5d3dd0707f73040deb4319109fed38865c43bc44
Sokhibjon Orzikulov <genemators@gmail.com>
2020-09-24 09:55:20 +0500

    database intialization

---
+++ database/db.js
@@ -0,0 +1,5 @@
+// Admins list database
+exports.users = require("./json/users.json");
+
+// Links database
+exports.links = require("./json/links.json");
---
+++ database/ds.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+
+module.exports = (link) => {
+  return axios
+    .get(link)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((errors) => {
+      return null;
+    });
+};
