02e764ffc1e6c17d86c7a485b8b7d484896a26e4
Sokhibjon Orzikulov <genemators@gmail.com>
2020-09-24 10:17:36 +0500

    keyboards markup ready

---
+++ layouts/keyboards.js
@@ -0,0 +1,27 @@
+const { Markup } = require("telegraf");
+
+exports.start = Markup.inlineKeyboard([
+  [Markup.callbackButton("Show more information", "help")],
+]);
+
+exports.help = Markup.inlineKeyboard([
+  [Markup.urlButton("Update Channel", "https://t.me/joinchat/AAAAAFkWg9XgdWv0c4Qs8Q")],
+]);
+
+exports.invalid = Markup.inlineKeyboard([
+  Markup.callbackButton(`Show available commands`, `help`),
+]);
+
+exports.error_admin = Markup.inlineKeyboard([
+  Markup.urlButton(`Contact with admin`, `https://t.me/genemator`),
+]);
+
+exports.links = async (links) => {
+  const keyboard = [];
+  for (let link of links) {
+    keyboard.push([
+      Markup.urlButton(link["name"], `https://t.me/joinchat/` + link["url"]),
+    ]);
+  }
+  return Markup.inlineKeyboard(keyboard);
+};
