1603a333480e3433d37df997909b7b19b20c1ca2
Sokhibjon Orzikulov <genemators@gmail.com>
2020-09-29 06:01:39 +0500

    Update chisel.js

--- action/middlewares/chisel.js
+++ action/middlewares/chisel.js
@@ -4,7 +4,7 @@ const consoles = require("../../layouts/consoles");
 const message = require("../../layouts/messages");
 const keyboard = require("../../layouts/keyboards");
 
-composer.command(`contribute`, async (ctx) => {
+composer.command(`chisel`, async (ctx) => {
   await ctx.replyWithHTML(message.chisel, {
     parse_mode: "HTML",
     reply_markup: keyboard.chisel,
